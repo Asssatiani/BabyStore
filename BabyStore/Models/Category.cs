3b06e9fa2cd553f0408a258469208e2945b74bed
l.asatiani <l.asatiani@LEVAN-1813>
2018-04-16 14:27:25 +0400

    ch4

--- BabyStore/Models/Category.cs
+++ BabyStore/Models/Category.cs
@@ -9,6 +9,10 @@ namespace BabyStore.Models
     {
         public int ID { get; set; }
 
+
+        [Required(ErrorMessage = "The category name cannot be blank")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Please enter a category namebetween 3 and 50 characters in length" )]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$", ErrorMessage = "Please enter a categoryname beginning with a capital letter and made up of letters and spaces only")]
         [Display(Name = "Category Name")]
         public string Name { get; set; }
 
--- BabyStore/Models/Product.cs
+++ BabyStore/Models/Product.cs
@@ -9,11 +9,26 @@ namespace BabyStore.Models
     {
         public int ID { get; set; }
 
+
+        [Required(ErrorMessage = "The product name cannot be blank")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Please enter a product namebetween 3 and 50 characters in length")]
+        [RegularExpression(@"^[a-zA-Z0-9'-'\s]*$", ErrorMessage = "Please enter a product namemade up of letters and numbers only")]
         [Display(Name = "Product Name")]
         public string Name { get; set; }
 
+
+        [Required(ErrorMessage = "The product description cannot be blank")]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "Please enter a productdescription between 10 and 200 characters in length")]
+        [RegularExpression(@"^[,;a-zA-Z0-9'-'\s]*$", ErrorMessage = "Please enter a productdescription made up of letters and numbers only")]
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
 
+
+        [Required(ErrorMessage = "The price cannot be blank")]
+        [Range(0.10, 10000, ErrorMessage = "Please enter a price between 0.10 and 10000.00")]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        [RegularExpression("[0-9]+(\\.[0-9][0-9]?)?", ErrorMessage = "The price must be a number up to two decimal places")]
         public decimal Price { get; set; }
 
         public int? CategoryID { get; set; }
