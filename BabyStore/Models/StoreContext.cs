bb0aa91cfe7db15ae2d9b19e71b190b01a6770f8
l.asatiani <l.asatiani@LEVAN-1813>
2018-04-16 12:37:15 +0400

    chapter1-3

---
+++ BabyStore/Models/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BabyStore.Models;
+using System.Data.Entity;
+namespace BabyStore.Models
+{
+    public class StoreContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
